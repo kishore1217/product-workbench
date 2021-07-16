5117a9195cf228749e9d7b03f9ddcbeab280c62b
kishore1217 <ganta.kishorebabu1234@gmail.com>
2021-07-16 22:57:26 +0530

    Release-2.2 Print statement added

---
+++ SpringBootExceptionHandling-1/src/main/java/com/nit/exception/MyExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.nit.exception;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Controller
+@ControllerAdvice
+public class MyExceptionHandler {
+
+	@ExceptionHandler(value = NullPointerException.class)
+	public String handleNullPointerException(Model model) {
+		
+		model.addAttribute("errorMsg", "Problem Occured, Please Try Later...");
+		
+		return"errorMsg";
+	}
+	
+	@ExceptionHandler(value = NoBookFoundException.class)
+	public String handleNoBookFoundException(Model model) {
+		model.addAttribute("error", "Sorry, No Book Found With The Given Id..!!");
+		return"customError";
+	}
+	
+
+}
